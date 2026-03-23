a4a8ba22fd97e27b926ce0494159313fbf311ad9
Mihai Valcu <valcu@orn.mpg.de>
2026-03-23 17:49:28 +0100

    chore: update database structure and UI components for 2026 season

--- main/global.R
+++ main/global.R
@@ -1,98 +1,117 @@
 #+ NOTE:
-  #' list.files('./R', full.names = TRUE) |> lapply(source) |> invisible(); source('global.R')
-  #' ss = function() shiny::runApp(launch.browser = TRUE)
+#' list.files('./R', full.names = TRUE) |> lapply(source) |> invisible(); source('global.R')
+#' ss = function() shiny::runApp(launch.browser = TRUE)
 
 SERVER = "de_fieldworker" # dbo::my.cnf()
 
 
 #! PACKAGES
-  sapply(
-    c(
-      "dbo",
-      "sf",
-      "data.table",
-      "stringr",
-      "forcats",
-      "zip",
-      "glue",
-      "ggplot2",
-      "ggrepel",
-      "ggtext",
-      "patchwork",
-      "shinyWidgets",
-      "bs4Dash",
-      "DT",
-      "leaflet",
-      "leafem",
-      "leaflet.extras"
-    ), require,
-    character.only = TRUE,
-    quietly = TRUE
-  )
+sapply(
+  c(
+    "dbo",
+    "sf",
+    "data.table",
+    "stringr",
+    "forcats",
+    "zip",
+    "glue",
+    "ggplot2",
+    "ggrepel",
+    "ggtext",
+    "patchwork",
+    "shinyWidgets",
+    "bs4Dash",
+    "DT",
+    "leaflet",
+    "leafem",
+    "leaflet.extras"
+  ),
+  require,
+  character.only = TRUE,
+  quietly = TRUE
+)
 
-  # other packages: wadeR, configr
+# other packages: wadeR, configr,shinycssloaders, ggpubr
 
 # External data
-  data(OsterFeinerMoor, package = "wadeR")
+data(OsterFeinerMoor, package = "wadeR")
 
 
 #- VARIABLES
 
-  cnf       = configr::read.config(getOption("dbo.my.cnf"))[[SERVER]]
-  user      = cnf$user
-  host      = cnf$host
-  pwd       = cnf$password
-  db        = cnf$database
-  dbbasenam = 'NOLAatDUMMERSEE'
-  years     = c(2024, 2025)
+cnf = configr::read.config(getOption("dbo.my.cnf"))[[SERVER]]
+user = cnf$user
+host = cnf$host
+pwd = cnf$password
+db = cnf$database
+dbbasenam = 'NOLAatDUMMERSEE'
+years = c(2024, 2025, 2026)
 
-  app_nam     = "DE_FIELDWORKER"
-  dbtabs_entry =
-    c("AUTHORS", "CAPTURES", "RESIGHTINGS", "CHICKS", "NESTS", "EGGS", "SAMPLES", "todo_white_list")
-  dbtabs_view =
-    c("AUTHORS", "CAPTURES", "CAPTURES_ARCHIVE", "RESIGHTINGS", "CHICKS", "NESTS", "EGGS", "SAMPLES", "COMBOS")
-  species              = c("NOLA", "REDS")
-  studySiteCenter      = c(x = 8.341151, y = 52.55065)
-
-
-  hatch_pred_gam = "./data/gam_float_to_hach.rds"
-
-
-  nest_state_cols = c(
-    "F"    = "#00815f",  
-    "C"    = "#E69F00",  
-    "I"    = "#fff023",  
-    "pP"   = "#A50026",  
-    "P"    = "#6405a3",  
-    "pD"   = "#CC79A7",  
-    "D"    = "#6A51A3",  
-    "H"    = "#1aa9fc",  
-    "notA" = "#4b4b4b"   
+app_nam = "DE_FIELDWORKER"
+dbtabs_entry =
+  c(
+    "AUTHORS",
+    "CAPTURES",
+    "RESIGHTINGS",
+    "CHICKS",
+    "NESTS",
+    "EGGS",
+    "SAMPLES",
+    "todo_white_list"
   )
-
-  todo_cols = c(
-    "catch M"    = "#0745cc",
-    "catch F"    = "#f33b0c",
-    "catch any"  = "#f38c38"
+dbtabs_view =
+  c(
+    "AUTHORS",
+    "CAPTURES",
+    "CAPTURES_ARCHIVE",
+    "RESIGHTINGS",
+    "CHICKS",
+    "NESTS",
+    "EGGS",
+    "SAMPLES",
+    "COMBOS"
   )
+species = c("NOLA", "REDS")
+studySiteCenter = c(x = 8.341151, y = 52.55065)
 
-  todo_symbols = c(
-      "nest check"  = 2,
-      "hatch check" = 5
-  )
 
+hatch_pred_gam = "./data/gam_float_to_hach.rds"
+
+
+nest_state_cols = c(
+  "F" = "#00815f",
+  "C" = "#E69F00",
+  "I" = "#fff023",
+  "pP" = "#A50026",
+  "P" = "#6405a3",
+  "pD" = "#CC79A7",
+  "D" = "#6A51A3",
+  "H" = "#1aa9fc",
+  "notA" = "#4b4b4b"
+)
+
+todo_cols = c(
+  "catch M" = "#0745cc",
+  "catch F" = "#f33b0c",
+  "catch any" = "#f38c38"
+)
+
+todo_symbols = c(
+  "nest check" = 2,
+  "hatch check" = 5
+)
 
 
 #! OPTIONS
-  options(shiny.autoreload = TRUE)
-  options(dbo.tz = "Europe/Berlin")
-  
-  options(ggrepel.max.overlaps = Inf)
+options(shiny.autoreload = TRUE)
+options(dbo.tz = "Europe/Berlin")
+
+options(ggrepel.max.overlaps = Inf)
 
 #! UI DEFAULTS
-  
-  ver                 = "2.2.0"
-  apptitle            = "DÜMMER-SEE"
-  pagetitle           = apptitle
-  set_capturedDaysAgo = 3
-  set_seenDaysAgo     = 3
+
+ver = "3.0.0"
+apptitle = "DÜMMER-SEE"
+pagetitle = apptitle
+set_capturedDaysAgo = 3
+set_seenDaysAgo = 3
--- main/ui.R
+++ main/ui.R
@@ -1,235 +1,271 @@
 bs4Dash::dashboardPage(
-scrollToTop = TRUE,
-dark = FALSE,
-help = NULL,
-preloader = list(
-  html = waiter::spin_loaders(id = 16, color = "#1e3d24"), 
-  color = '#2aa8a0'
-),
+  scrollToTop = TRUE,
+  dark = FALSE,
+  help = NULL,
+  preloader = list(
+    html = waiter::spin_loaders(id = 16, color = "#1e3d24"),
+    color = '#b38f96'
+  ),
 
-title = glue('FIELDWORKER {ver}'),
+  title = glue('FIELDWORKER {ver}'),
 
-header = dashboardHeader(
-  title = dashboardBrand(
-    title = paste(pagetitle, year(Sys.Date())),
-    image = "ICO.png"
-  ), 
-  uiOutput("ref_date_text")
-),
+  header = dashboardHeader(
+    title = dashboardBrand(
+      title = paste(pagetitle, year(Sys.Date())),
+      image = "ICO.png"
+    ),
+    uiOutput("ref_date_text")
+  ),
 
-sidebar = dashboardSidebar(
-  collapsed = TRUE,
-  sidebarMenu(
-    id = "main",  # Assigning an id here allows input$main to be set
-    menuItem("Start",         tabName = "start",         icon = icon("circle-play")),
-    menuItem("GPS",           tabName = "gps",           icon = icon("location-arrow")),
-    menuItem("Enter Data",    tabName = "enter_data",    icon = icon("edit")),
-    menuItem("Database",      tabName = "database",      icon = icon("database")),
-    menuItem("View Data",     tabName = "view_data",     icon = icon("table")),
-    menuItem("Nests Map",     tabName = "nests_map",     icon = icon("map")),
-    menuItem("Live Nest Map", tabName = "live_nest_map", icon = icon("broadcast-tower")),
-    menuItem("To-Do list",    tabName = "todo_list",     icon = icon("tasks")),
-    menuItem("To-Do map",     tabName = "todo_map",      icon = icon("street-view")),
-    menuItem("Overview",      tabName = "overview",      icon = icon("chart-line")),
-    menuItem("Hatching",      tabName = "hatching_est",  icon = icon("egg")), 
-    HR(),
-    menuItem( text = "",  icon = icon("github",style = "color: gray;"), 
-      href = "https://github.com/mpio-be/DE_FIELDWORKER"
-    ), 
-    menuItem( text = "",  icon = icon("at",style = "color: gray;"), 
-      href = "mailto:mihai.valcu@bi.mpg.de?subject=Complain"
-    )
-
-  )
-),
-
-controlbar = dashboardControlbar(
-  width = 400,
-  overlay = FALSE, 
-  collapsed = FALSE,
-  
-  box(title = "Reference date" |>bttl(), 
-    width = 12,collapsible = FALSE,  
-    dateInput(
-      inputId     = 'refdate', 
-      label       = NULL, 
-      value       = Sys.time() 
-    )
-
-  )
-  , 
-
-  box(title = "Map settings"|>bttl() , width = 12, 
-    sliderInput(
-      inputId = "nest_size",
-      label = "Text and symbol size:",
-      min = 3, max = 12, step = 0.5, value = 4
+  sidebar = dashboardSidebar(
+    collapsed = TRUE,
+    sidebarMenu(
+      id = "main", # Assigning an id here allows input$main to be set
+      menuItem("Start", tabName = "start", icon = icon("circle-play")),
+      menuItem("GPS", tabName = "gps", icon = icon("location-arrow")),
+      menuItem("Enter Data", tabName = "enter_data", icon = icon("edit")),
+      menuItem("Database", tabName = "database", icon = icon("database")),
+      menuItem("View Data", tabName = "view_data", icon = icon("table")),
+      menuItem("Nests Map", tabName = "nests_map", icon = icon("map")),
+      menuItem(
+        "Live Nest Map",
+        tabName = "live_nest_map",
+        icon = icon("broadcast-tower")
+      ),
+      menuItem("To-Do list", tabName = "todo_list", icon = icon("tasks")),
+      menuItem("To-Do map", tabName = "todo_map", icon = icon("street-view")),
+      menuItem("Overview", tabName = "overview", icon = icon("chart-line")),
+      menuItem("Hatching", tabName = "hatching_est", icon = icon("egg")),
+      HR(),
+      menuItem(
+        text = "",
+        icon = icon("github", style = "color: gray;"),
+        href = "https://github.com/mpio-be/DE_FIELDWORKER"
+      ),
+      menuItem(
+        text = "",
+        icon = icon("at", style = "color: gray;"),
+        href = "mailto:mihai.valcu@bi.mpg.de?subject=Complain"
       )
-    ,
+    )
+  ),
 
-    pickerInput(
-      inputId = "nest_state", label = "Nest state:",
-      multiple = TRUE,
-      choices = c(
-        "Found"             = "F",
-        "Collected"         = "C",
-        "Incubated"         = "I",
-        "possibly Predated" = "pP",
-        "possibly Deserted" = "pD",
-        "Predated"          = "P",
-        "Deserted"          = "D",
-        "Hatched"           = "H",
-        "Not Active"        = "notA"),
-      selected = c("F", "C", "I", "pP", "pD", "P", "D", "H", "notA")
+  controlbar = dashboardControlbar(
+    width = 400,
+    overlay = FALSE,
+    collapsed = FALSE,
+
+    box(
+      title = "Reference date" |> bttl(),
+      width = 12,
+      collapsible = FALSE,
+      dateInput(
+        inputId = 'refdate',
+        label = NULL,
+        value = Sys.time()
       )
-  )  
-  ,
+    ),
 
-  box(title = "Download" |>bttl(), width = 12, 
+    box(
+      title = "Map settings" |> bttl(),
+      width = 12,
+      sliderInput(
+        inputId = "nest_size",
+        label = "Text and symbol size:",
+        min = 3,
+        max = 12,
+        step = 0.5,
+        value = 4
+      ),
 
-    downloadBttn(outputId = "map_nests_pdf", label = "All nests", icon = icon("file-pdf")), 
-    downloadBttn(outputId = "map_todo_pdf", label = "To-do", icon = icon("file-pdf"))
-    
+      pickerInput(
+        inputId = "nest_state",
+        label = "Nest state:",
+        multiple = TRUE,
+        choices = c(
+          "Found" = "F",
+          "Collected" = "C",
+          "Incubated" = "I",
+          "possibly Predated" = "pP",
+          "possibly Deserted" = "pD",
+          "Predated" = "P",
+          "Deserted" = "D",
+          "Hatched" = "H",
+          "Not Active" = "notA"
+        ),
+        selected = c("F", "C", "I", "pP", "pD", "P", "D", "H", "notA")
+      )
+    ),
 
-  )
-  ,
-  uiOutput("clock")
-  ,
-  uiOutput("hdd_state")
-)
-, 
-body = dashboardBody(
-  includeCSS("./www/style.css"), 
+    box(
+      title = "Download" |> bttl(),
+      width = 12,
 
-  tags$script(HTML('
+      downloadBttn(
+        outputId = "map_nests_pdf",
+        label = "All nests",
+        icon = icon("file-pdf")
+      ),
+      downloadBttn(
+        outputId = "map_todo_pdf",
+        label = "To-do",
+        icon = icon("file-pdf")
+      )
+    ),
+    uiOutput("clock"),
+    uiOutput("hdd_state")
+  ),
+  body = dashboardBody(
+    includeCSS("./www/style.css"),
+
+    tags$script(HTML(
+      '
     $(function () {
       $("[data-toggle=\'popover\']").popover({ html: true });
     });
-  ')), 
+  '
+    )),
 
-  tabItems(
-    # Start tab (k4)
+    tabItems(
+      # Start tab (k4)
       tabItem(
-      tabName = "start",
+        tabName = "start",
         box(
-        collapsible = FALSE,
-        icon = icon("envelope-open"), 
-        width = 12,
-        includeMarkdown("./www/help/news.md")
-      )
-    
-    ),
-    # GPS tab
+          collapsible = FALSE,
+          icon = icon("envelope-open"),
+          width = 12,
+          includeMarkdown("./www/help/news.md")
+        )
+      ),
+      # GPS tab
       tabItem(
-      tabName = "gps",
+        tabName = "gps",
         includeMarkdown("./www/help/gps.md"),
         uiOutput("open_gps")
-    ),
-    # Enter Data tab
+      ),
+      # Enter Data tab
       tabItem(
-      tabName = "enter_data",
+        tabName = "enter_data",
         uiOutput("new_data"),
         hr(),
         includeMarkdown("./www/help/enter_data.md")
-    ),
-    # DB tab
+      ),
+      # DB tab
       tabItem(
-      tabName = "database",
-      uiOutput("open_db"),
-      includeMarkdown("./www/help/database.md")
-    ),
-    # View Data tab
+        tabName = "database",
+        uiOutput("open_db"),
+        includeMarkdown("./www/help/database.md")
+      ),
+      # View Data tab
       tabItem(
-      tabName = "view_data",
+        tabName = "view_data",
         bs4Dash::tabsetPanel(
           id = "tabset",
           .list = lapply(dbtabs_view, function(i) {
             tabPanel(
               title = paste0("[", i, "]"),
               active = FALSE,
-              spinner( 
+              spinner(
                 DT::DTOutput(outputId = paste0(i, "_show"))
-              )  
+              )
             )
           })
         )
-    ),
-    # Nests Map tab
+      ),
+      # Nests Map tab
       tabItem(
-      tabName = "nests_map",
-      fluidRow(
-        box(width = 12, maximizable = TRUE,
-          spinner(
-            plotOutput("map_nests_show")
-          )
-        
-        )
-      )
-    ),
-    # Live Nest Map tab
-      tabItem(
-      tabName = "live_nest_map",
-      fluidRow(
-        box(
-          width = 12,
-          maximizable = TRUE,
-          
-          spinner(
-          leafletOutput(outputId = "nest_dynmap_show",width = "100%", height = "calc(99vh - 1px)") 
-          )
-
-        )
-      )
-    ),
-    # To-Do list tab
-      tabItem(
-      tabName = "todo_list",
-      spinner(
-      DT::DTOutput(outputId = "todo_list_show") 
-      )
-    ), 
-    # Overview tab
-      tabItem(
-      tabName = "overview",
-      spinner(
-        plotOutput("overview_show")
-      )
-    ), 
-    # To-Do map tab
-      tabItem(
-      tabName = "todo_map",
-      fluidRow(
-        box(width = 12, maximizable = TRUE,
-          
-          spinner(
-            plotOutput("map_todo_show")
-          )
-        
-        )
-      )
-    ),
-    # Hatching tab
-      tabItem(
-      tabName = "hatching_est",
-
-      fluidRow(
-        box(title = 'Select flotation ',icon = icon("gears"), width = 2,
-          sliderInput('float_angle', 'Angle:', value = 50, min = 14, max = 90,step = 1),
-          sliderInput('float_height', 'Height:',value = 2, min = 0, max = 6,step = 1)
-        ), 
-
-        box(width = 8,
-          spinner(
-            plotOutput(outputId = "hatching_est_plot") 
+        tabName = "nests_map",
+        fluidRow(
+          box(
+            width = 12,
+            maximizable = TRUE,
+            spinner(
+              plotOutput("map_nests_show")
             )
+          )
+        )
+      ),
+      # Live Nest Map tab
+      tabItem(
+        tabName = "live_nest_map",
+        fluidRow(
+          box(
+            width = 12,
+            maximizable = TRUE,
+
+            spinner(
+              leafletOutput(
+                outputId = "nest_dynmap_show",
+                width = "100%",
+                height = "calc(99vh - 1px)"
+              )
+            )
+          )
+        )
+      ),
+      # To-Do list tab
+      tabItem(
+        tabName = "todo_list",
+        spinner(
+          DT::DTOutput(outputId = "todo_list_show")
+        )
+      ),
+      # Overview tab
+      tabItem(
+        tabName = "overview",
+        spinner(
+          plotOutput("overview_show")
+        )
+      ),
+      # To-Do map tab
+      tabItem(
+        tabName = "todo_map",
+        fluidRow(
+          box(
+            width = 12,
+            maximizable = TRUE,
+
+            spinner(
+              plotOutput("map_todo_show")
+            )
+          )
+        )
+      ),
+      # Hatching tab
+      tabItem(
+        tabName = "hatching_est",
+
+        fluidRow(
+          box(
+            title = 'Select flotation ',
+            icon = icon("gears"),
+            width = 2,
+            sliderInput(
+              'float_angle',
+              'Angle:',
+              value = 50,
+              min = 14,
+              max = 90,
+              step = 1
+            ),
+            sliderInput(
+              'float_height',
+              'Height:',
+              value = 2,
+              min = 0,
+              max = 6,
+              step = 1
+            )
+          ),
+
+          box(
+            width = 8,
+            spinner(
+              plotOutput(outputId = "hatching_est_plot")
+            )
+          )
         )
       )
-
     )
-
   )
-)
-
-
-
 )
